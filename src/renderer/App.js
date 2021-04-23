df60076b94a05b06b5d496a2ffdd382aab7e5687
Abda Shaffan <abdashaffan@gmail.com>
2021-04-23 11:05:47 +0700

    add main canvas library

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,52 +1,13 @@
 import { hot } from 'react-hot-loader/root'
 
-import requireStatic from '@/requireStatic'
 import React from 'react'
 import './App.css'
-
-const reactLogo = requireStatic('./react.svg')
-const electronLogo = requireStatic('electron.png')
-
-// eslint-disable-next-line react/prop-types
-const Link = React.memo(function Link({ href, children }) {
-  return (
-    <a
-      rel="noopener noreferrer"
-      target="_blank"
-      className="App-link"
-      href={href}
-    >
-      {children}
-    </a>
-  )
-})
+import Canvas from "./components/Canvas";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={reactLogo}
-          className="App-logo react-logo"
-          alt="react-logo logo"
-        />
-        <img
-          src={electronLogo}
-          className="App-logo electron-logo"
-          alt="electron logo"
-        />
-      </header>
-      <p>
-        Edit <code>src/renderer/App.js</code> and save to reload.
-      </p>
-      <p>
-        <Link href="https://reactjs.org">Learn React</Link>
-      </p>
-      <p>
-        <Link href="https://webpack.electron.build/">
-          Learn Electron webpack
-        </Link>
-      </p>
+      <Canvas />
     </div>
   )
 }
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,23 +1,14 @@
 /// <reference types="hypermerge/src/types/hyperswarm" />
 
+// You need to whitelist every newly added module inside electron-webpack.json
+// Relevant thread: https://github.com/expo/expo-cli/issues/2835#issuecomment-722952296
+
 import '@/env'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import {Doc} from 'automerge';
-import {DocUrl, Repo} from 'hypermerge';
-import Hyperswarm from 'hyperswarm';
 
-const swarm = Hyperswarm({queue: {multiplex: true}});
-const repo = new Repo({memory: true});
-repo.addSwarm(swarm, {announce: true});
-
-const url = repo.create({
-    objects: {},
-    peers: {}
-});
-console.log(url);
 
 ReactDOM.render(<App />, document.getElementById('app'))
