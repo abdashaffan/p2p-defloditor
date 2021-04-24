b89859b15e75a9b653ba662deb53ce5d73422623
Abda Shaffan <abdashaffan@gmail.com>
2021-04-24 23:22:26 +0700

    change env path

--- src/common/env.js
+++ src/common/env.js
@@ -11,7 +11,7 @@ export const USER = process.env.USER || os.hostname();
 // We want these constants available from both the main and render threads.
 const app = electron.app || electron.remote.app
 export const DATA_PATH = app.getPath('userData')
-export const USER_PATH = Path.join(DATA_PATH, 'defloditor', USER)
+export const USER_PATH = Path.join(DATA_PATH, 'p2p-defloditor-react', USER)
 export const HYPERMERGE_PATH = Path.join(USER_PATH, 'hypermerge')
 export const LAST_WORKSPACE_URL_PATH = Path.join(HYPERMERGE_PATH, 'url.json');
 
