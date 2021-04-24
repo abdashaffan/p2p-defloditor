330a7627259cecb03835ac999850d7a520b2c04f
Abda Shaffan <abdashaffan@gmail.com>
2021-04-24 10:22:28 +0700

    change crdt state representation

--- src/renderer/Hooks.js
+++ src/renderer/Hooks.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {v4 as uuidv4} from 'uuid';
 import {Repo} from 'hypermerge';
 import Hyperswarm from 'hyperswarm';
-import {starterElements} from "./starter";
+import {initialElements} from "./starter";
 
 // Idea: Simpen class Hypermerge di dalem context, panggil disini
 // Only use on the root component
@@ -15,8 +15,14 @@ class Hypermerge {
     this.repo.addSwarm(this.swarm, {announce: true});
     // TODO: Implement peer mechanism
     this.url = this.repo.create({
-      elements: starterElements,
-      peers: []
+      elements: initialElements,
+      peers: {
+        1: {
+          id: 1,
+          name: 'wild-racoon',
+          color: "#12345"
+        }
+      }
     });
     this.doc = null;
     this.watch(callback);
@@ -28,9 +34,18 @@ class Hypermerge {
       console.log('[hypermerge watch triggered]');
       this.doc = state;
       if (callback) {
-        console.log('new state:');
-        console.log(state);
-        callback(state);
+        const newlocalState = {
+          elements: [],
+          peers: []
+        }
+        Object.keys(state.elements).forEach(key => {
+          newlocalState.elements.push(state.elements[key]);
+        });
+        Object.keys(state.peers).forEach(key => {
+          newlocalState.peers.push(state.peers[key]);
+        });
+        console.log(newlocalState);
+        callback(newlocalState);
       }
     });
   }
@@ -59,7 +74,7 @@ export const useEntityManager = () => {
       data: {label: 'New Node'}
     }
     hypermerge.update(state => {
-      state.elements.push(newNode);
+      state.elements[newNode.id] = newNode;
     });
   }
 
@@ -68,46 +83,29 @@ export const useEntityManager = () => {
     const targetId = params.target;
 
     return hypermerge.update(state => {
-      const srcNode = state.elements.find(el => el.id === srcId);
-      const targetNode = state.elements.find(el => el.id === targetId);
       // Only add edge if the source and the target node are still exist.
-      if (srcNode && targetNode) {
+      if (state.elements[srcId] && state.elements[targetId]) {
         const newEdge = {
           id: `edge:${uuidv4()}`,
           source: srcId,
           target: targetId
         }
-        state.elements.push(newEdge);
+        state.elements[newEdge.id] = newEdge;
       }
     });
-
   }
 
   const deleteShape = (elementsToRemove) => {
     // Original react-flow API supports deleting multiple apps at once,
     // but this app only supports one shape per deletion.
     const idToBeRemoved = elementsToRemove[0].id;
-    // Need to do it like this, using built-in array.filter() API will cause error on the automerge side.
     return hypermerge.update(state => {
-      let deleteNodeIdx = -1;
-      let deleteEdgeIdxList = [];
-      for (let i = 0; i < state.elements.length; i++) {
-        // Search elements to be deleted.
-        if (state.elements[i].id === idToBeRemoved) {
-          deleteNodeIdx = i;
-          break;
+      Object.keys(state.elements).forEach(id => {
+        const el = state.elements[id];
+        // Remove the node and all connected edges to said node.
+        if (el.id === idToBeRemoved || el.source === idToBeRemoved || el.target === idToBeRemoved) {
+          delete state.elements[id];
         }
-      }
-      delete state.elements[deleteNodeIdx];
-
-      for (let i = 0; i < state.elements.length; i++) {
-        // And also search all edges that directly connected to also be deleted.
-        if (state.elements[i].source == idToBeRemoved || state.elements[i].target == idToBeRemoved) {
-          deleteEdgeIdxList.push(i);
-        }
-      }
-      deleteEdgeIdxList.forEach(idx => {
-        delete state.elements[idx];
       })
     });
   }
--- src/renderer/starter.js
+++ src/renderer/starter.js
@@ -1,15 +1,14 @@
-export const starterElements = [
-  {
+export const initialElements = {
+  1: {
     id: '1',
     type: 'input', // input node
     data: {label: 'Welcome to defloditor'},
     position: {x: 250, y: 25},
     style: {borderColor: 'green'}
   },
-  // default node
-  {
+  2: {
     id: '2',
     data: {label: 'A P2P flowchart editor'},
     position: {x: 100, y: 125},
   }
-];
+}
