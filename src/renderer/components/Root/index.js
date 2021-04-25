92eef3ccb2a537d1c16ce2ba0f2f10bbe7ca2b22
Abda Shaffan <abdashaffan@gmail.com>
2021-04-26 00:36:29 +0700

    add loader

--- src/renderer/components/Root/index.js
+++ src/renderer/components/Root/index.js
@@ -9,11 +9,13 @@ import {ReactFlowProvider} from "react-flow-renderer";
 import {Container, Row} from "react-bootstrap";
 import env from "../../../common/env";
 import CustomAvatarGroup from "../AvatarGroup";
+import {LinearProgress} from "@material-ui/core";
 
 // Save document in user's local in production.
 // const withPersistence = env.isProduction;
 const withPersistence = false;
 const module = env.module;
+
 function Root() {
 
   const {
@@ -28,7 +30,7 @@ function Root() {
     validateUrl,
     getUrl,
     getUser
-  } = useEntityManager(withPersistence,module);
+  } = useEntityManager(withPersistence, module);
 
   const getAnnotatedPeers = (peers) => {
     if (peers.length < 1) return peers;
@@ -39,35 +41,37 @@ function Root() {
     })
   }
 
-  return (
-    <ReactFlowProvider>
-      <Container className="root">
-        <Row className="mt-5"><h2>Basic Flowchart Editor</h2></Row>
-        <Row className="mt-3">
-          <UrlInput handleUrlUpdate={updateUrl} validateUrl={validateUrl}/>
-        </Row>
-        <Row className="mt-3">
-          <h5>Current Workspace Url:</h5>
-        </Row>
-        <Row><p><b>{getUrl()}</b></p></Row>
-        <Row className="mt-3"><h5>Users in this document: {peers.length}</h5></Row>
-        <Row>
-          <CustomAvatarGroup peers={getAnnotatedPeers(peers)}/>
-        </Row>
-        <Row className="canvas-container mt-5 mb-5">
-          <Canvas
-            elements={elements}
-            handleRemove={deleteShape}
-            handleAddEdge={addNewEdge}
-            handleNodeUpdate={updateNode}
-            handleEdgeUpdate={updateEdgeConnection}
-            handleAddNode={addNewShape}
-          />
-        </Row>
-      </Container>
-    </ReactFlowProvider>
-
-  )
+  if (peers && peers.length > 0) {
+    return (
+      <ReactFlowProvider>
+        <Container className="root">
+          <Row className="mt-5"><h2>Basic Flowchart Editor</h2></Row>
+          <Row className="mt-3">
+            <UrlInput handleUrlUpdate={updateUrl} validateUrl={validateUrl}/>
+          </Row>
+          <Row className="mt-3">
+            <h5>Current Workspace Url:</h5>
+          </Row>
+          <Row><p><b>{getUrl()}</b></p></Row>
+          <Row className="mt-3"><h5>Users in this document: {peers.length}</h5></Row>
+          <Row>
+            <CustomAvatarGroup peers={getAnnotatedPeers(peers)}/>
+          </Row>
+          <Row className="canvas-container mt-5 mb-5">
+            <Canvas
+              elements={elements}
+              handleRemove={deleteShape}
+              handleAddEdge={addNewEdge}
+              handleNodeUpdate={updateNode}
+              handleEdgeUpdate={updateEdgeConnection}
+              handleAddNode={addNewShape}
+            />
+          </Row>
+        </Container>
+      </ReactFlowProvider>
+    )
+  }
+  return <LinearProgress />;
 }
 
 
