c98b0fb47f83976e327f4f976919d7048b04cc10
Abda Shaffan <abdashaffan@gmail.com>
2021-05-24 22:27:33 +0700

    implement testable module for yjs

---
+++ test/YjsSync.js
@@ -0,0 +1,136 @@
+const Y = require('yjs');
+const util = require('util');
+const {isANode, ITEM} = require("./utils");
+
+
+const ELEMENTS_KEY = 'elements';
+
+class YjsSync {
+
+  constructor() {
+    this.ydoc = new Y.Doc();
+  }
+
+  addElement(elArr) {
+    this.ydoc.transact(() => {
+      elArr.forEach(el => {
+        let newEl = new Y.Map();
+        if (isANode(el)) {
+          newEl.set(ITEM.ID, el.id);
+          newEl.set(ITEM.LABEL, el.data.label);
+          newEl.set(ITEM.TYPE, "default");
+          newEl.set(ITEM.POSITION, el.position);
+          newEl.set(ITEM.BACKGROUND, el.style.backgroundColor);
+          newEl.set(ITEM.BORDER, el.style.borderColor);
+        } else {
+          newEl.set(ITEM.ID, el.id);
+          newEl.set(ITEM.SOURCE, el.source);
+          newEl.set(ITEM.TARGET, el.target);
+        }
+        this.ydoc.getMap(ELEMENTS_KEY).set(el.id, newEl);
+      })
+    });
+  }
+
+  updateElement(elArr) {
+    this.ydoc.transact(() => {
+      elArr.forEach(el => {
+        let updatable;
+        let updatableKey;
+        if (isANode(el)) {
+          updatable = [el.position, el.style.borderColor, el.style.backgroundColor];
+          updatableKey = [ITEM.POSITION, ITEM.BORDER, ITEM.BACKGROUND];
+        } else {
+          updatable = [el.source, el.target];
+          updatableKey = [ITEM.SOURCE, ITEM.TARGET];
+        }
+        for (let i = 0; i < updatable.length; i++) {
+          const lastVal = this.ydoc.getMap(ELEMENTS_KEY).get(el.id).get(updatableKey[i]);
+          const newVal = updatable[i];
+          if (newVal !== lastVal) {
+            this.ydoc.getMap(ELEMENTS_KEY).get(el.id).set(updatableKey[i], newVal);
+          }
+        }
+      })
+    });
+  }
+
+  deleteElement(idArr) {
+    this.ydoc.transact(() => {
+      idArr.forEach(id => {
+        this.ydoc.getMap(ELEMENTS_KEY).delete(id);
+      });
+      this.removeOrphanedEdge();
+    });
+  }
+
+  removeOrphanedEdge() {
+    const nodeCount = {};
+    const edgeList = [];
+    this.ydoc.getMap(ELEMENTS_KEY).forEach(el => {
+      const currId = el.get(ITEM.ID);
+      if (el.has(ITEM.SOURCE)) {
+        edgeList.push(el);
+      }
+      if (!nodeCount[currId]) {
+        nodeCount[currId] = 1;
+      } else {
+        nodeCount[currId]++;
+      }
+    });
+    edgeList.forEach(edge => {
+      if (!nodeCount[edge.get(ITEM.SOURCE)] || !nodeCount[edge.get(ITEM.TARGET)]) {
+        this.ydoc.getMap(ELEMENTS_KEY).delete(edge.get(ITEM.ID));
+      }
+    });
+  }
+
+  sync(otherYdoc) {
+    const changes = Y.encodeStateAsUpdate(otherYdoc);
+    Y.applyUpdate(this.ydoc, changes);
+  }
+
+  getCrdtState() {
+    return this.ydoc;
+  }
+
+  getState() {
+    return {elements: this._getElements(), peers: []};
+  }
+
+  show(label) {
+    console.log('\x1b[33m%s\x1b[0m',`\n\n\n[${label.toUpperCase()}:]`);
+    console.log(util.inspect(this.getState(), {showHidden: false, depth: null}));
+  }
+
+  _getElements() {
+    const elementArrCopy = [];
+    const elementsYmap = this.ydoc.getMap(ELEMENTS_KEY);
+    elementsYmap.forEach((el, key) => {
+      const deepCopy = JSON.parse(JSON.stringify(el));
+      let newElement;
+      if (deepCopy[ITEM.ID].startsWith('node')) {
+        newElement = {
+          id: deepCopy[ITEM.ID],
+          type: deepCopy[ITEM.TYPE],
+          data: {label: deepCopy[ITEM.LABEL]},
+          position: deepCopy[ITEM.POSITION],
+          style: {
+            backgroundColor: deepCopy[ITEM.BACKGROUND],
+            borderColor: deepCopy[ITEM.BORDER]
+          }
+        }
+      } else {
+        newElement = {
+          id: deepCopy[ITEM.ID],
+          source: deepCopy[ITEM.SOURCE],
+          target: deepCopy[ITEM.TARGET]
+        }
+      }
+      elementArrCopy.push(newElement);
+    });
+    return elementArrCopy;
+  }
+}
+
+module.exports = YjsSync;
--- test/index.js
+++ test/index.js
@@ -1,11 +1,21 @@
 const {initialElements} = require('./utils');
 const AutomergeSync = require("./AutomergeSync");
-
-
+const YjsSync = require("./YjsSync");
 
 const starters = Object.values(initialElements);
 console.table(starters);
-const userA = new AutomergeSync();
+
+// const userA = new AutomergeSync("user-1");
+// userA.show("initial data");
+// userA.addElement(starters);
+// userA.show("after adding elements");
+// userA.deleteElement(['node:1']);
+// userA.show("after del node:1");
+// starters[1].position = {x: 0, y:0};
+// userA.updateElement([starters[1]]);
+// userA.show("after changing pos");
+
+const userA = new YjsSync();
 userA.show("initial data");
 userA.addElement(starters);
 userA.show("after adding elements");
