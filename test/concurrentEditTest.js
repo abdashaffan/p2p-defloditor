2cbbfda1f1c6a4bfa7829bb7a8bfadfaca26f10a
Abda Shaffan <abdashaffan@gmail.com>
2021-05-26 14:54:18 +0700

    set multiple test cases

--- test/concurrentEditTest.js
+++ test/concurrentEditTest.js
@@ -14,7 +14,16 @@ const syncChanges = (user1, user2, callback) => {
   if (callback) callback();
 }
 
-function runTest(user1, user2, starterElements, label) {
+function runTest(userList, starterElements) {
+
+  const broadcastChangesToEachOther = (userList, callback) => {
+    for (let i = 0; i < userList.length; i++) {
+      for (let j = 0; j < userList.length; j++) {
+        if (i != j) syncChanges(userList[i], userList[j]);
+      }
+    }
+    if (callback) callback();
+  }
 
   let data = {
     docSizeInBytes: 0,
@@ -22,83 +31,84 @@ function runTest(user1, user2, starterElements, label) {
     execTimeInMs: 0
   }
 
-  syncChanges(user1, user2);
+  userList.forEach(user => {
+    user.addElement(starterElements);
+  });
+  broadcastChangesToEachOther(userList);
 
-  user1.addElement(starterElements);
-  user2.addElement(starterElements);
-  syncChanges(user1, user2);
 
-  const changeForUser1 = starterElements[0];
-  const changeForUser2 = starterElements[0];
-
-  changeForUser1[ITEM.BACKGROUND] = "green";
-  changeForUser2[ITEM.BACKGROUND] = "yellow";
-
-  user1.updateElement([changeForUser1]);
-  user2.updateElement([changeForUser2]);
+  userList.forEach(user => {
+    const temp = starterElements[0];
+    temp[ITEM.BACKGROUND] = Math.floor(Math.random() * 16777215).toString(16);
+    user.updateElement([temp]);
+  });
 
   const initialMemUsed = getMemUsedInMb(true);
   const initialTime = performance.now();
-  syncChanges(user1, user2, () => {
+  broadcastChangesToEachOther(userList, () => {
     data.execTimeInMs = performance.now() - initialTime;
     data.memUsedInMb = getMemUsedInMb(false) - initialMemUsed;
-    data.docSizeInBytes = Math.max(user1.getDocSizeInBytes(), user2.getDocSizeInBytes());
+    userList.forEach(user => {
+      data.docSizeInBytes = Math.max(data.docSizeInBytes, user.getDocSizeInBytes());
+    });
   });
   return data;
 }
 
-const concurrentEditPerformanceTest = () => {
+const concurrentEditPerformanceTest = ({testCases, numTrial}) => {
 
-  const NUM_TRIAL = 100;
+  testCases.forEach(totalUser => {
 
-  const automergeAvgData = {
-    docSizeInBytes: 0,
-    memUsedInMb: 0,
-    execTimeInMs: 0
-  }
+    const automergeAvgData = {
+      docSizeInBytes: 0,
+      memUsedInMb: 0,
+      execTimeInMs: 0
+    }
 
-  const yjsAvgData = {
-    docSizeInBytes: 0,
-    memUsedInMb: 0,
-    execTimeInMs: 0
-  }
+    const yjsAvgData = {
+      docSizeInBytes: 0,
+      memUsedInMb: 0,
+      execTimeInMs: 0
+    }
 
-  for (let i = 1; i <= NUM_TRIAL; i++) {
+    for (let i = 1; i <= numTrial; i++) {
+      let automergeUsers = [];
+      let yjsUsers = [];
+      for (let j = 0; j < totalUser; j++) {
+        automergeUsers.push(new AutomergeSync(`user-${j + 1}`));
+        yjsUsers.push(new YjsSync());
+      }
 
-    const user1Automerge = new AutomergeSync("user-1");
-    const user2Automerge = new AutomergeSync("user-2");
-    const user1Yjs = new YjsSync();
-    const user2Yjs = new YjsSync();
-    const starterElements = generateNewElements(1);
+      const starterElements = generateNewElements(1);
 
 
-    const aData = runTest(user1Automerge, user2Automerge, starterElements);
-    const yjsData = runTest(user1Yjs, user2Yjs, starterElements);
+      const aData = runTest(automergeUsers, starterElements);
+      const yjsData = runTest(yjsUsers, starterElements);
+
+      Object.keys(automergeAvgData).forEach(metric => {
+        automergeAvgData[metric] += aData[metric];
+      });
+
+      Object.keys(yjsAvgData).forEach(metric => {
+        yjsAvgData[metric] += yjsData[metric];
+      });
+
+    }
 
     Object.keys(automergeAvgData).forEach(metric => {
-      automergeAvgData[metric] += aData[metric];
+      automergeAvgData[metric] /= numTrial;
     });
 
     Object.keys(yjsAvgData).forEach(metric => {
-      yjsAvgData[metric] += yjsData[metric];
+      yjsAvgData[metric] /= numTrial;
     });
 
-  }
+    console.log(`AUTOMERGE AVG FOR ${numTrial} TRIALS WITH ${totalUser} USERS:`);
+    console.table(automergeAvgData);
 
-  Object.keys(automergeAvgData).forEach(metric => {
-    automergeAvgData[metric] /= NUM_TRIAL;
+    console.log(`YJS AVG FOR ${numTrial} TRIALS WITH ${totalUser} USERS:`);
+    console.table(yjsAvgData);
   });
-
-  Object.keys(yjsAvgData).forEach(metric => {
-    yjsAvgData[metric] /= NUM_TRIAL;
-  });
-
-  console.log(`AUTOMERGE AVG FOR ${NUM_TRIAL} TRIALS:`);
-  console.table(automergeAvgData);
-
-  console.log(`YJS AVG FOR ${NUM_TRIAL} TRIALS:`);
-  console.table(yjsAvgData);
-
 }
 
 module.exports = concurrentEditPerformanceTest;
