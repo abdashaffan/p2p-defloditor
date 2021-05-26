9a8c11e84fd90daddeda4cedd2741ad97d6aee0c
Abda Shaffan <abdashaffan@gmail.com>
2021-05-26 15:27:06 +0700

    change param

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-const numTrial = 100;
+const numTrial = 10;
 const numElements = [1,10,50,100,200];
-const numUsers = [2,5,10,20];
+const numUsers = [2,5,10,20,50];
 
 require("./addElementsTest")({ testCases: numElements, numTrial });
 require("./deleteElementsTest")({ testCases: numElements, numTrial });
