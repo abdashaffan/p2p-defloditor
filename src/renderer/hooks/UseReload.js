00f71af3eb19fc7ba5d382ac45995a4d960a6289
Abda Shaffan Diva <abdashaffan@gmail.com>
2021-05-27 08:37:06 +0700

    remove log

--- src/renderer/hooks/UseReload.js
+++ src/renderer/hooks/UseReload.js
@@ -1,53 +1,49 @@
 import { useState, useEffect, useRef } from 'react';
 import isOnline from 'is-online';
-const {getCurrentWindow} = require('electron').remote;
+const { getCurrentWindow } = require('electron').remote;
 
 const CONN_CHECK_INTERVAL = 2000;
 
 export const UseReload = () => {
-    
-    const [online, setOnline] = useState(true);
-    const [showReloadBtn, setShowReloadBtn] = useState(false);
+  const [online, setOnline] = useState(true);
+  const [showReloadBtn, setShowReloadBtn] = useState(false);
 
-    const reload = () => {
-      getCurrentWindow().reload();
-    }
+  const reload = () => {
+    getCurrentWindow().reload();
+  };
 
-    useInterval(() => {
+  useInterval(() => {
+    (async () => {
+      let isNowOnline = await isOnline();
+      const isReconnected = !online && isNowOnline;
+      if (isReconnected) {
+        setShowReloadBtn(true);
+      } else {
+        setOnline(isNowOnline);
+      }
+    })();
+  }, CONN_CHECK_INTERVAL);
 
-      (async () => {
-        let isNowOnline = await isOnline();
-        const isReconnected = !online && isNowOnline;
-        if (isReconnected) {
-            setShowReloadBtn(true);
-        } else {
-          setOnline(isNowOnline);
-        }
-        console.log('online:', isNowOnline);  
-      })();
-
-    }, CONN_CHECK_INTERVAL);
-
-    return {showReloadBtn, reload};
-}
+  return { showReloadBtn, reload };
+};
 
 // source: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 function useInterval(callback, delay) {
-    const savedCallback = useRef();
-  
-    // Remember the latest callback.
-    useEffect(() => {
-      savedCallback.current = callback;
-    }, [callback]);
-  
-    // Set up the interval.
-    useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    }, [delay]);
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
